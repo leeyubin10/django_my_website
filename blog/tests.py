fcea9dfcae4e44146e040cdc5fbfbab213f9503d
Yubin Lee <lybin10@naver.com>
2020-07-19 10:28:34 +0900

    categories in post_detail page

--- blog/tests.py
+++ blog/tests.py
@@ -66,6 +66,11 @@ class TestView(TestCase):
         self.assertIn('Blog', navbar.text)
         self.assertIn('About me', navbar.text)
 
+    def check_right_side(self, soup):
+        category_card = soup.find('div', id='category-card')
+        self.assertIn('미분류 (1)', category_card.text)  # 미분류 (1) 있어야 함
+        self.assertIn('정치/사회 (1)', category_card.text)  # 정치/사회 (1) 있어야 함
+
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
@@ -108,20 +113,25 @@ class TestView(TestCase):
         self.assertEqual(post_000_read_more_btn['href'], post_000.get_absolute_url())
 
         # category card 에서
-        category_card = body.find('div', id='category-card')
-        self.assertIn('미분류 (1)', category_card.text)    # 미분류 (1) 있어야 함
-        self.assertIn('정치/사회 (1)', category_card.text)    # 정치/사회 (1) 있어야 함
-
+        self.check_right_side(soup)
+        
         # main div에는
-        main_div = body.find('div', id='main_div')
-        self.assertIn('정치/사회', main_div.text)    # 첫번째 포스트에는 '정치/사회 있어야 함
-        self.assertIn('미분류', main_div.text)    # main div에는 '미분류' 있어야 함
+        main_div = soup.find('div', id='main_div')
+        self.assertIn('정치/사회', main_div.text)  # 첫번째 포스트에는 '정치/사회 있어야 함
+        self.assertIn('미분류', main_div.text)  # main div에는 '미분류' 있어야 함
 
     def test_post_detail(self):
         post_000 = create_post(
             title='The first post',
             content='Hello World. We are the world.',
             author=self.author_000,
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='정치/사회')
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -145,3 +155,5 @@ class TestView(TestCase):
         self.assertIn(post_000.author.username, main_div.text)
 
         self.assertIn(post_000.content, main_div.text)
+
+        self.check_right_side(soup)
